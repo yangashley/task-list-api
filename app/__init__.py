e499cd10304ad5335ab2cbc053023352f2aa65c3
simon <simon@adadevelopersacademy.org>
2021-04-15 14:53:12 -0700

    adds test config scaffold to create_app

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,20 @@ from flask import Flask
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    app.debug = True
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    if test_config is None:
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_DATABASE_URI")
+    else:
+        app.config["TESTING"] = True
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_TEST_DATABASE_URI")
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # Register Blueprints here
 
     return app
