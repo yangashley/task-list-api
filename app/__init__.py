3719903e77983deb6ee4f0b810edd516968e8958
simon <simon@adadevelopersacademy.org>
2021-04-15 15:38:08 -0700

    adds imports for sqlalchemy, migrate, dotenv into app/init for scaffold

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+from dotenv import load_dotenv
+
+
+db = SQLAlchemy()
+migrate = Migrate()
+load_dotenv()
 
 
 def create_app(test_config=None):
