fcc70507930a5125d0ffae5464f8a60547e8427f
simon <simon@adadevelopersacademy.org>
2021-04-16 01:50:24 -0700

    adds in model imports for alembic purposes

--- app/__init__.py
+++ app/__init__.py
@@ -23,6 +23,10 @@ def create_app(test_config=None):
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
             "SQLALCHEMY_TEST_DATABASE_URI")
 
+    # Import models here for Alembic setup
+    from app.models.task import Task
+    from app.models.goal import Goal
+
     db.init_app(app)
     migrate.init_app(app, db)
 
