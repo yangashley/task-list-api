9f129d3591f5ee105fdc43a2fd0baa06ad530450
Becca Elenzil <becca@adadevelopersacademy.org>
2022-04-07 13:52:44 -0700

    remove solution

--- app/__init__.py
+++ app/__init__.py
@@ -7,28 +7,22 @@ load_dotenv()  # take environment variables from .env.
 db = SQLAlchemy()
 migrate = Migrate()
 
+
 def create_app(test_config=None):
     app = Flask(__name__)
 
     if not test_config:
         app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/tasks_list_api_development'    
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/tasks_list_api_development'
     else:
         app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/tasks_list_api_test'    
-    
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/tasks_list_api_test'
+
+    # Import models here for Alembic setup
+    from app.models.task import Task
+    from app.models.goal import Goal
+
     db.init_app(app)
     migrate.init_app(app, db)
-
-    from .routes import tasks_bp, goals_bp, root_bp
-    app.register_blueprint(tasks_bp)
-    app.register_blueprint(goals_bp)
-    app.register_blueprint(root_bp)
-
-    from .routes import root_bp
-    app.register_blueprint(root_bp)
-
-    # with app.app_context():
-    #     db.create_all()
 
     return app
--- app/models/goal.py
+++ app/models/goal.py
@@ -1,12 +1,5 @@
 from app import db
 
-class Goal(db.Model):
-    goal_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.String)
-    tasks = db.relationship('Task', backref='goal', lazy=True)
 
-    def to_json(self):
-        return {
-            "id": self.goal_id,
-            "title": self.title,
-        }
+class Goal(db.Model):
+    goal_id = db.Column(db.Integer, primary_key=True)
