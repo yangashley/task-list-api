485515a6dd45e6d3a2af5b4b3e3e4ac006e65b81
Becca Elenzil <becca@adadevelopersacademy.org>
2022-04-07 13:56:16 -0700

    update init file to use env

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,26 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import os
 from dotenv import load_dotenv
 
-load_dotenv()  # take environment variables from .env.
+
 db = SQLAlchemy()
 migrate = Migrate()
+load_dotenv()
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-    if not test_config:
-        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/tasks_list_api_development'
+    if test_config is None:
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_DATABASE_URI")
     else:
-        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/tasks_list_api_test'
+        app.config["TESTING"] = True
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_TEST_DATABASE_URI")
 
     # Import models here for Alembic setup
     from app.models.task import Task
@@ -24,5 +28,7 @@ def create_app(test_config=None):
 
     db.init_app(app)
     migrate.init_app(app, db)
+
+    # Register Blueprints here
 
     return app
